5bb940fb20bc3939cf7f6577a075e16f5fddc9ba
BergSabbath <Bergit7@gmail.com>
2023-08-31 16:04:24 -0300

    adicionado o arquivo filme.java

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,62 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    //inincio dos getters and setters
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento){
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public int getDuracaoEmMinutos(){
+        return duracaoEmMinutos;
+    }
+
+    public boolean getIncluidoNoPlano(){
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNPlano){
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos){
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+//fim do getters and setters
+
+    public void exibeFichaTecnica(){
+        System.out.println(nome);
+        System.out.println(anoDeLancamento);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
