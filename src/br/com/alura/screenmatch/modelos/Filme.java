c4c3d08e5052c028db043263e4f9fef5d548f453
BergSabbath <Bergit7@gmail.com>
2023-09-01 10:30:16 -0300

    criada classe titulo e feita a extensão nas classes filme e serie

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,62 +1,14 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+public class Filme extends Titulo {
+    private String diretor;
 
-    //inincio dos getters and setters
-    public String getNome(){
-        return nome;
+    public String getDiretor(){
+        return diretor;
     }
 
-    public void setNome(String nome){
-        this.nome = nome;
+    public void setDiretor(String diretor){
+        this.diretor = diretor;
     }
 
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento){
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public int getDuracaoEmMinutos(){
-        return duracaoEmMinutos;
-    }
-
-    public boolean getIncluidoNoPlano(){
-        return incluidoNoPlano;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNPlano){
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos){
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
-    }
-//fim do getters and setters
-
-    public void exibeFichaTecnica(){
-        System.out.println(nome);
-        System.out.println(anoDeLancamento);
-    }
-
-    public void avalia(double nota) {
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-    }
 }
