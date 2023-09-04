a67a27dd18805a6e3c54e773240932e22d9de512
BergSabbath <Bergit7@gmail.com>
2023-09-04 12:36:47 -0300

    criação de novos métodos e implementação do polimorfismo

--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,42 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
 
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefão");
-        meuFilme.setAnoDeLancamento(1970);
-        meuFilme.setDuracaoEmMinutos(180);
-        System.out.println("Duração do filme " + meuFilme.getDuracaoEmMinutos() );
+//        Filme meuFilme = new Filme();
+//        meuFilme.setNome("O poderoso chefão");
+//        meuFilme.setAnoDeLancamento(1970);
+//        meuFilme.setDuracaoEmMinutos(180);
+//        System.out.println("Duração do filme " + meuFilme.getDuracaoEmMinutos() );
+
+        Filme favorito = new Filme();
+        favorito.setNome("The Matrix");
+        favorito.setAnoDeLancamento(1999);
+        favorito.setDuracaoEmMinutos(135);
+        favorito.setIncluidoNoPlano(true);
+
+        Filme outro = new Filme();
+        outro.setNome("John Wick");
+        outro.setAnoDeLancamento(2014);
+        outro.setDuracaoEmMinutos(101);
+        outro.setIncluidoNoPlano(true);
+
+        Serie serie = new Serie();
+        serie.setNome("La casa de Papel");
+        serie.setAnoDeLancamento(2017);
+        serie.setIncluidoNoPlano(true);
+        serie.setAtiva(true);
+        serie.setTemporadas(5);
+        serie.setEpisodiosPorTemporadas(10);
+        serie.setMinutosPorEpisodio(45);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(favorito);
+        calculadora.inclui(outro);
+        calculadora.inclui(serie);
+        System.out.println("Tempo total:" + calculadora.getTempoTotal());
 
 
        /* meuFilme.exibeFichaTecnica();
@@ -18,13 +46,13 @@ public class Principal {
         System.out.println("Total de avaliações " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());*/
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(2000);
-        lost.exibeFichaTecnica();
-        lost.setTemporadas(10);
-        lost.setEpisodiosPorTemporadas(10);
-        lost.setMinutosPorEpisodio(50);
-        System.out.println("Duração para maratonar Lost " + lost.getDuracaoEmMinutos() );
+//        Serie lost = new Serie();
+//        lost.setNome("Lost");
+//        lost.setAnoDeLancamento(2000);
+//        lost.exibeFichaTecnica();
+//        lost.setTemporadas(10);
+//        lost.setEpisodiosPorTemporadas(10);
+//        lost.setMinutosPorEpisodio(50);
+//        System.out.println("Duração para maratonar Lost " + lost.getDuracaoEmMinutos() );
     }
 }
