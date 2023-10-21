e8dfefe29ee877db5f8822af1e30f4868a9b0b9f
BergSabbath <Bergit7@gmail.com>
2023-10-20 22:35:38 -0300

    utilizando record class

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -18,6 +18,12 @@ public class Titulo  implements Comparable<Titulo> {
         this.anoDeLancamento = anoDeLancamento;
     }
 
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
+    }
+
     //inincio dos getters and setters
     public String getNome(){
         return nome;
@@ -82,6 +88,6 @@ public class Titulo  implements Comparable<Titulo> {
     @Override
     public String toString() {
         return "nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento;
+                ", anoDeLancamento=" + anoDeLancamento + ", duração=" + duracaoEmMinutos;
     }
 }
